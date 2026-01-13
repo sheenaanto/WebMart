894bb2c9ce0031377b9f1cea11237be5785d1925
sheenaanto <sheena.anto08@gmail.com>
2026-01-13 13:44:32 +0000

    Update: Allowed hosts

--- webmart/settings.py
+++ webmart/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-&i*8zuhp&xagawvj_)kyw333oc97u8x7d_37(^)an&loskszbg
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
 
-ALLOWED_HOSTS = ['herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition

0ebb7624760f48d5b9985a45bbbe55168186acc8
sheenaanto <sheena.anto08@gmail.com>
2026-01-13 19:30:24 +0000

    update Secret key

--- webmart/settings.py
+++ webmart/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&i*8zuhp&xagawvj_)kyw333oc97u8x7d_37(^)an&loskszbg'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False

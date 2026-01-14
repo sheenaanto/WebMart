39b6e3a80ae94193a2fb9b6530c5f736accdd2b6
sheenaanto <sheena.anto08@gmail.com>
2026-01-14 16:56:55 +0000

    Implement pagination for products display

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 
 from category.models import Category
 from .models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -11,12 +12,21 @@ def store(request, category_slug=None):
         selected_category = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(
             category=selected_category, is_available=True)
+        # pagination code
+        products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
         products_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        # pagination code
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+
         products_count = products.count()
     context = {
-        'products': products,
+        'products': paged_products,
         'products_count': products_count,
     }
     return render(request, 'store/store.html', context)

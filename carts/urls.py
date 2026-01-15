3706bd70fdd10efa8883cb9295fd2714b9d21da5
sheenaanto <sheena.anto08@gmail.com>
2026-01-15 14:17:20 +0000

    Add cart functionality with add_cart view and update templates for product detail and cart display

--- carts/urls.py
+++ carts/urls.py
@@ -5,5 +5,6 @@ from carts import views
 
 urlpatterns = [
     path('', views.cart, name='storecart'),
+    path('add/<int:product_id>/', views.add_cart, name='add_cart'),
 
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,49 @@
-from django.shortcuts import render
+from urllib import request
+from django.shortcuts import redirect, render
+
+from carts.models import Cart, CartItem
+from store.models import Product
 
 # Create your views here.
 
 
-def cart(request):
-    return render(request, 'store/carts.html')
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=_cart_id(request))
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product, cart=cart, quantity=1)
+    cart_item.save()
+    return redirect('storecart')
+
+
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+    except Cart.DoesNotExist:
+        pass
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'quantity': quantity,
+    }
+
+    return render(request, 'store/carts.html', context)

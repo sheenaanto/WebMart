4cacd4f50c9bc6653f7709ac5c82d787b7e6072b
sheenaanto <sheena.anto08@gmail.com>
2026-01-19 23:16:44 +0000

    Add search functionality

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from carts.models import CartItem
@@ -5,6 +6,7 @@ from carts.views import _cart_id
 from category.models import Category
 from .models import Product
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 
@@ -54,3 +56,20 @@ def product_detail(request, category_slug, product_slug):
     }
 
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            # Q()-combine multiple conditions using OR (|) or AND (&)
+            products = Product.objects.filter(
+                Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            products_count = products.count()
+            context = {
+                'products': products,
+                'products_count': products_count,
+            }
+            return render(request, 'store/store.html', context)
+        else:
+            return render(request, 'store/store.html')

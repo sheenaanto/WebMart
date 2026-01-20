4ac5b78047b34f5cd7dc1a7a2524e73b285fa002
sheenaanto <sheena.anto08@gmail.com>
2026-01-20 21:00:27 +0000

    Bug fix: update secret keyn

--- env.py
+++ env.py
@@ -4,7 +4,7 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_lmIRs4oNZX8q@ep-misty-shadow-agymb2hb.c-2.eu-central-1.aws.neon.tech/dried_foot_jab_46983")
 
 os.environ.setdefault(
-    "SECRET_KEY", "django-insecure-&i*8zuhp&xagawvj_)kyw333oc97u8x7d_37(^)an&loskszbg")
+    "SECRET_KEY", "django-insecure-&i*8zuhp&xagawvj_)kyw333oc97u8x7d_37(^)asdan&loskszbg")
 
 os.environ.setdefault(
     "CLOUDINARY_URL", "cloudinary://512514582279566:-cjZryTIVZlYkmBudI4DdizeRow@diwnqyu5g")

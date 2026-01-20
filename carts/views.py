35d41ac8e3c748d09f1c8be34c1fc3f083faa0aa
sheenaanto <sheena.anto08@gmail.com>
2026-01-20 20:55:11 +0000

    Bug fix : Remove button

--- carts/views.py
+++ carts/views.py
@@ -75,6 +75,7 @@ def remove_cart_item(request, product_id, cart_item_id):
         cart_item = CartItem.objects.get(
             product=product, user=request.user, id=cart_item_id)
     else:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_item = CartItem.objects.get(
             product=product, cart=cart, id=cart_item_id)
     cart_item.delete()
